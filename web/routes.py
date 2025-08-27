7e48432364b75606a9c77217c7c2b19df4d7ed36
mortalgaming4u <mortalgaming4u@gmail.com>
2025-08-27 09:02:01 +0530

    Update routes.py

--- web/routes.py
+++ web/routes.py
@@ -1,1 +1,13 @@
-e
+from flask import render_template
+from writer import get_chapter_list, get_chapter_content
+
+def register_routes(app):
+    @app.route("/")
+    def home():
+        chapters = get_chapter_list()
+        return render_template("toc.html", chapters=chapters)
+
+    @app.route("/chapter/<int:num>")
+    def chapter(num):
+        content = get_chapter_content(num)
+        return render_template("chapter.html", content=content)
